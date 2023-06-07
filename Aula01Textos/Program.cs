4c2615eb235965cfb60b8f46d87d7a49848dbacf
Stella Gonçalves <135771572+estelar2003@users.noreply.github.com>
2023-06-07 17:26:46 -0300

    Add files via upload

---
+++ Aula01Textos/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01Textos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+            
+            Console.WriteLine("ola mundo");
+            Console.WriteLine("estou \n aprendendo C#");
+            Console.WriteLine("sou a stella");
+            Console.WriteLine("the office");
+
+            Console.Write("\n \n ");
+            Console.WriteLine("ola mundo \n \n ");
+            Console.WriteLine("aprendendo \n c# \n \n ");
+            Console.WriteLine("meu nome é stella");
+            Console.Write("isso é legal"); 
+
+
+
+
+
+
+          
+            // o comando Console.WriteLine serve para mostrar um texto na tela
+            Console.WriteLine("\n");
+            // o n\ é para pular uma linha no texto
+            // dá pra usar 2 n\ tbm, aí pula 2 linhas
+            // eu posso escrever quantas WriteLines eu quiser, repetidas vezes
+            Console.WriteLine("Bem-Vindo a cafeteria da stellita: \n \n ");
+            Console.WriteLine("Essas são as nossas opções:");
+            Console.WriteLine("- café R$ 2,00");
+            // lalalkakdkdklallal
+            Console.WriteLine("- capuccino R$ 3,50");
+            Console.WriteLine("- mocaccino R$ 2.000,00");
+            Console.Write("\n \n \n escolha uma opção e dirija-se ao caixa...");
+            //xou da xuxa
+            // tbm tem o comando Console.Write(" ola, mundo");
+            // mas ele não pula linha 
+            // SEMPRE usar ; no final 
+            // a barra invertida \ serve para escape
+            // para fazer aspas use \"
+
+            Console.ReadKey();
+
+
+
+
+
+
+        }
+    }
+}
